975c7523fe7b04527346e2b6df289b9556cc1c4a
Arthur Bowers <arthur.bowers@phabrix.com>
2024-08-28 16:19:16 +0100

    Finishing off implementation for general.rs
    * Added AttemptDespawn struct.
    * Added deferred_despawn_system function.
    * Added play_sfx function.

--- src/general.rs
+++ src/general.rs
@@ -5,3 +5,26 @@ use crate::Flags;
 pub(super) fn plugin(app: &mut App) {
     app.add_systems(PostUpdate, deferred_despawn_system);
 }
+
+#[derive(Component)]
+pub struct AttemptDespawn;
+
+fn deferred_despawn_system(
+    mut commands: Commands,
+    to_despawn_query: Query<Entity, With<AttemptDespawn>>,
+) {
+    for to_despawn in to_despawn_query.iter() {
+        if let Some(mut entity) = commands.get_entity(to_despawn) {
+            entity.despawn();
+        }
+    }
+}
+
+pub fn play_sfx(commands: &mut Commands, source: &Handle<AudioSource>, flags: &Flags) {
+    if !flags.muted {
+        commands.spawn(AudioBundle {
+            source: source.clone(),
+            settings: PlaybackSettings::DESPAWN,
+        });
+    }
+}
