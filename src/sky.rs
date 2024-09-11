125ddb6786865b4abb6e65d12e237ef76e478296
Arthur Bowers <arthur.bowers@phabrix.com>
2024-09-11 17:15:34 +0100

    Adding sky

--- src/sky.rs
+++ src/sky.rs
@@ -21,3 +21,23 @@ pub struct Sun;
 #[derive(Resource, Deref, DerefMut)]
 struct CycleTimer(Timer);
 
+fn setup_sky_system(mut commands: Commands) {
+    commands.spawn((Sun, DirectionalLightBundle::default()));
+}
+
+fn daylight_cycle_system(
+    mut atmosphere: AtmosphereMut<Nishita>,
+    mut query: Query<(&mut Transform, &mut DirectionalLight), With<Sun>>,
+    mut timer: ResMut<CycleTimer>,
+    time: Res<Time>,
+) {
+    timer.tick(time.delta());
+
+    if timer.finished() {
+        let t = time.elapsed_seconds_wrapped() / 24.0;
+        atmosphere.sun_position =Vec3::new(0., t.sin(), t.cos());
+
+        if let Some((mut light_trans))
+    }
+}
+
